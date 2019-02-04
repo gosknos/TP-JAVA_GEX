1723893805385bbc1799f33f1977c10ee806d053
gosknos <38354174+gosknos@users.noreply.github.com>
2019-02-04 11:10:47 +0100

    Add files via upload
    Fait avec Théo GAULLIER

---
+++ exo2/NegativeException.java
@@ -0,0 +1,13 @@
+package exo2;
+
+public class NegativeException extends Exception{
+
+		public NegativeException () {
+			System.out.println("le nombre est négatif");
+		}
+		
+		public String toString() 
+		{
+			return "le nombre est négatif";
+		}	
+}
